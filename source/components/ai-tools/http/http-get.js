2dc869651aa9bebbf29afd431663334a11224a81
franck gaspoz <franck.gaspoz@gmail.com>
2026-03-19 03:40:25 +0100

    fix com to work in file

--- source/components/ai-tools/http/http-get.js
+++ source/components/ai-tools/http/http-get.js
@@ -1,4 +1,6 @@
 import AITool from '../../ai/ai-tool';
+import { writeFileSync } from 'fs'
+
 const htmlparser2 = require('htmlparser2');
 
 // http get https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal, extract main text from page
@@ -23,30 +25,37 @@ export default class HttpGet extends AITool {
     specification() {
         return {
             name: "http_get",
-            description: "fetch a web page at url using http",
+            description: "download a web page or any document at url using http and save it into a file with given path and/or name",
             parameters: {
                 type: "object",
                 properties: {
                     "url": {
                         "type": "string"
+                    },
+                    "filepath": {
+                        "type": "string"
                     }
                 }
             },
-            required: ['url']
+            required: ['url', "filepath"]
         }
     }
 
     async run(args) {
         const url = args?.url
+        const path = args?.filepath
         var r = null
 
         try {
+            if (!url || url.length == 0) throw new Error('the tool argument "url" is missing')
             const response = await fetch(url);
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             const data = await response.text(); // Parse JSON response
-            r = data//this.getText(data)
+            if (!path || path.length == 0) throw new Error('the tool argument "filepath" is missing')
+            writeFileSync(path, data)
+            r = "the file has been correctly downloaded and saved to the path: " + path
 
         } catch (error) {
             r = 'Error fetching data:' + error.message;
