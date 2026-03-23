cdfff0e3515d4b3d2c17de73653e51c907d45537
franck gaspoz <franck.gaspoz@gmail.com>
2026-03-23 14:21:17 +0100

    add unic strip deco

--- source/config/config.js
+++ source/config/config.js
@@ -86,7 +86,8 @@ export default function config(cli) {
 					['✨', ' '],
 					['🚴‍♂', ' '],
 					['🌫', ' '],
-					['⚡', ' ']
+					['⚡', ' '],
+					['🏴‍☠️', ' ']
 				]
 			},
 			mouseScrollStep: 4,
